1498bca3eb36c78282b53ccb7b545b0da8f73c0e
amulay11 <abhishekmulay@gmail.com>
2024-02-17 15:34:53 +0800

    is model 2 ok ?

---
+++ ml_model.py
@@ -0,0 +1,12 @@
+print("Adding code for ml model")
+
+def model1():
+    print("Adding code for ML Model 1")
+
+def model2():
+    print("Adding code for ML Model 2")
+
+def model3():
+    print("Adding code for ML Model 3")
+
+print("model 2 is performing the best so use it - is it really ?")
