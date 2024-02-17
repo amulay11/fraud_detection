2025973ad682c8e1d9eeaf015987132815ca0835
amulay11 <abhishekmulay@gmail.com>
2024-02-17 14:12:46 +0800

    added function for ml model 1, 2, 3

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("Adding code for ml model")
+
+def model1():
+    print("Adding code for ML Model 1")
+
+def model2():
+    print("Adding code for ML Model 2")
+
+def model3():
+    print("Adding code for ML Model 3")
